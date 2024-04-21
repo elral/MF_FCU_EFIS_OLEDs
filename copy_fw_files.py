3b13b384940e2db05419bea6bb3fbdd1156c8d80
elral <3263285+elral@users.noreply.github.com>
2024-04-21 11:45:34 +0200

    change folder name in zip file, clean up device.ini and copy FW files (#10)
    New folder name in zip file, clean up

--- copy_fw_files.py
+++ copy_fw_files.py
@@ -13,7 +13,7 @@ firmware_version = firmware_version.strip(".")
 community_project = env.GetProjectOption('custom_community_project', "")
 
 # Get the custom folder from the build environment.
-custom_device_folder = env.GetProjectOption('custom_device_folder', "")
+custom_source_folder = env.GetProjectOption('custom_source_folder', "")
 
 # Get the foldername inside the zip file from the build environment.
 community_folder = env.GetProjectOption('custom_community_folder', "")
@@ -22,21 +22,20 @@ community_folder = env.GetProjectOption('custom_community_folder', "")
 def copy_fw_files (source, target, env):
     fw_file_name=str(target[0])
 
-    if os.path.exists("./_build/" + custom_device_folder) == False:
-        os.makedirs("./_build/" + custom_device_folder + "/Community/firmware")
-        shutil.copytree(custom_device_folder + "/Community", "./_build/" + custom_device_folder + "/Community", dirs_exist_ok=True)
-        print("Creating Folder and copying community folder")
+    if os.path.exists("./_build/" + custom_source_folder) == False:
+        os.makedirs("./_build/" + custom_source_folder + "/Community/firmware")
+        shutil.copytree(custom_source_folder + "/Community", "./_build/" + custom_source_folder + "/Community", dirs_exist_ok=True)
+        print("Creating /_build folder")
     
     if fw_file_name[-3:] == "bin":
         fw_file_name=fw_file_name[0:-3] + "uf2"
 
-    shutil.copy(fw_file_name, "./_build/" + custom_device_folder + "/Community/firmware")
-    createCommunityZipFile(source, target, env)
-
-def createCommunityZipFile(source, target, env):
-    original_folder_path = "./_build/" + custom_device_folder + "/Community"
+    print("Copying community folder")
+    shutil.copy(fw_file_name, "./_build/" + custom_source_folder + "/Community/firmware")
+    original_folder_path = "./_build/" + custom_source_folder + "/Community"
     zip_file_path = './_dist/' + community_project + '_' + firmware_version + '.zip'
     new_folder_in_zip = community_folder
+    print("Creating zip file")
     createZIP(original_folder_path, zip_file_path, new_folder_in_zip)
 
 def createZIP(original_folder_path, zip_file_path, new_folder_name):
@@ -53,5 +52,3 @@ def createZIP(original_folder_path, zip_file_path, new_folder_name):
 
 env.AddPostAction("$BUILD_DIR/${PROGNAME}.hex", copy_fw_files)
 env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", copy_fw_files)
-#env.AddPostAction("checkprogsize", createCommunityZipFile)
-#env.AddCustomTarget("create_community_zip", None, createCommunityZipFile)
