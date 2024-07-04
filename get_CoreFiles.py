2df965ec587d510ea34b83ee4fff7908eafd0503
elral <3263285+elral@users.noreply.github.com>
2024-07-04 11:06:09 +0200

    consider white spaces in folder path

--- get_CoreFiles.py
+++ get_CoreFiles.py
@@ -2,16 +2,17 @@ import os
 
 Import("env")
 
+CORESOURCE = "https://github.com/MobiFlight/MobiFlight-FirmwareSource"
 CORESOURCE_DIR = env.subst("$PROJECT_DIR/src")
 CORESOURCE_TAG = env.GetProjectOption("custom_core_firmware_version")
 print("Compiling for Core Version: " + CORESOURCE_TAG)
 
 if not os.path.exists(CORESOURCE_DIR):
-    print ("Cloning Mobiflight-CustomDevices repo ... ")
-    env.Execute(f"git clone --depth 1 --filter=blob:none --sparse --branch {CORESOURCE_TAG} https://github.com/MobiFlight/MobiFlight-FirmwareSource $PROJECT_DIR/src")
-    env.Execute(f"git --work-tree=$PROJECT_DIR/src --git-dir=$PROJECT_DIR/src/.git sparse-checkout set _Boards src")
+    print ("Cloning Mobiflight-Firmware repo ... ")
+    env.Execute(f'git clone --depth 1 --filter=blob:none --sparse --branch {CORESOURCE_TAG} "{CORESOURCE}" "{CORESOURCE_DIR}"')
+    env.Execute(f'git --work-tree="{CORESOURCE_DIR}" --git-dir="{CORESOURCE_DIR}/.git" sparse-checkout set _Boards src')
     if os.path.isfile(CORESOURCE_DIR + "/platformio.ini"):
         os.remove(CORESOURCE_DIR + "/platformio.ini")
 else:
-    print ("Checking for Mobiflight-CustomDevices repo updates ... ")
-    env.Execute(f"git --work-tree=$PROJECT_DIR/src --git-dir=$PROJECT_DIR/src/.git pull origin {CORESOURCE_TAG} --depth 100")
+    print ("Checking for Mobiflight-Firmware repo updates ... ")
+    env.Execute(f'git --work-tree="{CORESOURCE_DIR}" --git-dir="{CORESOURCE_DIR}/.git" pull origin {CORESOURCE_TAG} --depth 100')
